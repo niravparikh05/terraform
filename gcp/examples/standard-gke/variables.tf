acd7fb61302ccfe8776f30394e8e059f8e77111f
Nirav Parikh <52062717+niravparikh05@users.noreply.github.com>
2024-04-26 17:54:12 +0530

    Update variables.tf

--- gcp/examples/standard-gke/variables.tf
+++ gcp/examples/standard-gke/variables.tf
@@ -17,7 +17,7 @@ variable "region" {
 variable "zones" {
   type        = list(string)
   description = "The zones to host the cluster in (optional if regional cluster / required if zonal)"
-  default     = "us-central1-a"
+  default     = ["us-central1-a"]
 }
 variable "network" {
   type        = string
