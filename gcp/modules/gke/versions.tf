96528a75f94615d17a888e4291a0192b58ba744b
Nirav Parikh <52062717+niravparikh05@users.noreply.github.com>
2024-04-26 11:23:15 +0000

    updated hashicorp/google plugin version to ">=5.9.0"

--- gcp/modules/gke/versions.tf
+++ gcp/modules/gke/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.74.0"
+      version = ">=5.9.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
