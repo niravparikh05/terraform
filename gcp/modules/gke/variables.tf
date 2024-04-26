32a209f0efa0e82160bc38eb2aa17f337774f877
Nirav Parikh <52062717+niravparikh05@users.noreply.github.com>
2024-04-26 17:58:57 +0530

    Update variables.tf

--- gcp/modules/gke/variables.tf
+++ gcp/modules/gke/variables.tf
@@ -90,8 +90,12 @@ variable "node_pools_oauth_scopes" {
   description = "Map of lists containing node oauth scopes by node-pool name"
   default = {
     all = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
     ]
   }
 }
